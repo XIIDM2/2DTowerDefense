31f6892a0d7ec790ce649bd40da870f0c096d23d
XIIDM2 <xiidm2@gmail.com>
2025-10-03 14:42:51 +0300

    new editor version

--- Assets/Scripts/Utilities/Singleton.cs
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -6,16 +6,41 @@ using UnityEngine;
 
 public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    public static T Instance { get; private set; }
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindFirstObjectByType<T>();
+            }
+
+            if ( _instance == null)
+            {
+                GameObject go = new GameObject(typeof(T).Name);
+                _instance = go.AddComponent<T>();
+            }
+
+            return _instance;
+        }
+    }
 
     protected virtual void Awake()
     {
-        Instance = this as T;
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this as T;
     }
 
     protected virtual void OnApplicationQuit()
     {
-        Instance = null;
+        _instance = null;
         Destroy(gameObject);
     }
 }
